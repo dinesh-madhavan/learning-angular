25bb38395554c39056d6491232e527a8585d5885
dinesh-madhavan <dinesh.jith98@gmail.com>
2020-09-22 11:22:33 -0700

    added components

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
---
+++ first-app/src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { mainModule } from 'process';
+
+@Component({
+    selector:"main-app",
+    templateUrl:"./main.component.html",
+    styleUrls:["./main.component.css"]
+})
+
+export class MainComponent{
+
+}
